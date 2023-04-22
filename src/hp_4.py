2def8182d9e9576c197e8c3478788cbec7b8ade0
Rahulvennela <128725670+Rahulvennela@users.noreply.github.com>
2023-04-22 21:21:16 +0530

    Update hp_4.py

--- src/hp_4.py
+++ src/hp_4.py
@@ -1,34 +1,63 @@
 # hp_4.py
 #
 from datetime import datetime, timedelta
+import pandas as pd
 from csv import DictReader, DictWriter
 from collections import defaultdict
 
-
 def reformat_dates(old_dates):
-    """Accepts a list of date strings in format yyyy-mm-dd, re-formats each
-    element to a format dd mmm yyyy--01 Jan 2001."""
-    pass
+    """ Reform The Dates"""
+    outl = []
+    for i in old_dates:
+        k = dt.datetime.strptime(i, '%Y-%m-%d')
+        outl.append(k.strftime('%d %b %Y'))
+    return outl
 
 
 def date_range(start, n):
-    """For input date string `start`, with format 'yyyy-mm-dd', returns
-    a list of of `n` datetime objects starting at `start` where each
-    element in the list is one day after the previous."""
-    pass
+    """Gets the Date Range """
+    if  not isinstance(start, str):
+        raise TypeError
+    elif not isinstance(n, int):
+        raise TypeError
+    else:
+        a1 = dt.datetime.strptime(start, '%Y-%m-%d')
+        k = [a1]
+        for i in range(n - 1):
+            a1 += dt.timedelta(days=1)
+            k.append(a1)
+        return k
 
 
 def add_date_range(values, start_date):
-    """Adds a daily date range to the list `values` beginning with
-    `start_date`.  The date, value pairs are returned as tuples
-    in the returned list."""
-    pass
+    """ Add Dat e Range """
+    date_val = date_range(start_date, len(values))
+    k = []
+    for i in range(len(values)):
+        k.append((date_val[i], values[i]))
+    return k
 
 
 def fees_report(infile, outfile):
-    """Calculates late fees per patron id and writes a summary report to
-    outfile."""
-    pass
+    """ Final Infile /Outfile Output"""
+    df = pd.read_csv(infile)
+    df = df.drop(['book_uid', 'isbn_13', 'date_checkout'], axis=1)
+    df['date_due'] = pd.to_datetime(df['date_due'], format="%m/%d/%Y")
+    df['date_returned'] = pd.to_datetime(df['date_returned'],
+                                         format="%m/%d/%Y")
+    df['extra_days'] = df['date_returned'] - df['date_due']
+    df['extra_days'] = df['extra_days'].map(
+                       lambda k: int(str(k).split(' ')[0]))
+    df['extra_days'] = df['extra_days'].map(lambda k: k if k > 0 else 0)
+    df['late_fees'] = df['extra_days'].map(lambda k: round(k*0.25, 2))
+    df.drop(['date_due', 'date_returned', 'extra_days'], axis=1, inplace=True)
+    df = df.groupby(['patron_id']).sum()
+    # return df
+    df.to_csv(outfile)
+
+
+
+
 
 
 # The following main selection block will only run when you choose
